c57e7d59e5c0167ee4ea9f7b2d981d7cb8fd6256
Navdata <nixc9631@gmail.com>
2022-05-16 12:25:54 -0400

    4 ejercicio

---
+++ los_cuadrados_de_un_numero.py
@@ -0,0 +1,27 @@
+
+def run():
+    LIMITE = 2*10**9
+    
+    contador = 0
+    pote_2 = 2**contador
+    while pote_2 < LIMITE:
+        print("2 elevado a " + str(contador) + " es igual a "+str(pote_2))
+        contador += 1
+        pote_2 = 2**contador
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    run()
